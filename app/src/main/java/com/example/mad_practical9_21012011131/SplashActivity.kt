218361265573d27ad68d46d959bbb5990c848f38
student <student@gnu.ac.in>
2023-10-17 10:48:12 +0530

    2

--- app/src/main/java/com/example/mad_practical9_21012011131/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical9_21012011131/SplashActivity.kt
@@ -1,22 +1,23 @@
 package com.example.mad_practical9_21012011131
 
-import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.WindowManager
 import android.view.animation.Animation
 import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
+        setContentView(R.layout.activity_splash)
+        val img: ImageView = findViewById(R.id.img)
+        val anim = img.drawable as AnimationDrawable
+        anim.start()
 
     }
+
 
 }
