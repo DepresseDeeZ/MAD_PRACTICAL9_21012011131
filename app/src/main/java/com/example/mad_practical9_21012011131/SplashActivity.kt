9bae91bf230c04795619e596179767b17c02b655
student <student@gnu.ac.in>
2023-10-17 10:43:39 +0530

    2

--- app/src/main/java/com/example/mad_practical9_21012011131/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical9_21012011131/SplashActivity.kt
@@ -11,15 +11,11 @@ import android.view.animation.Animation
 import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
-    //    lateinit var logoanimation: Animation
-//     val img: ImageView = findViewById(R.id.img)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-//        val img: ImageView = findViewById(R.id.img)
-//        img.setBackgroundResource(R.drawable.uvpce_animation_list)
-//        logoanimation =img.background as Animation
-//        logoanimation.start()
+
 
     }
 
