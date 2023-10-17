9bb30b6ed8dc9f85e0631a01c24f2029f494a326
student <student@gnu.ac.in>
2023-10-17 10:40:33 +0530

    2

--- app/src/main/java/com/example/mad_practical9_21012011131/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical9_21012011131/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      val intent = Intent(this,SplashActivity::class.java).also { startActivity(intent) }
-
+//        val intent = Intent(this, SplashActivity::class.java)
+//        startActivity(intent)
 
 
 
